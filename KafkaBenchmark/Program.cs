be1299065917aae4232b3af601165eddc872f9c3
Михаил Сафронов <muxaul@git.imbalanced.tech>
2024-10-08 22:45:57 +0300

    red panda added

--- KafkaBenchmark/Program.cs
+++ KafkaBenchmark/Program.cs
@@ -3,20 +3,21 @@ using KafkaBenchmark;
 
 class Program
 {
+  const string p_bootstrapServers = "localhost:9092s";
   static async Task Main(string[] args)
   {
     var producerConfig = CreateProducerConfig();
     var consumerConfig = CreateConsumerConfig("benchmark-group");
 
-    // await KafkaLatencyBenchmark.RunBenchmark(producerConfig, consumerConfig);
+    await KafkaLatencyBenchmark.RunBenchmark(producerConfig, consumerConfig);
     
-    await KafkaLagTest.RunLagTests(producerConfig, consumerConfig);
+    // await KafkaLagTest.RunLagTests(producerConfig, consumerConfig);
   }
 
   private static ProducerConfig CreateProducerConfig() =>
     new()
     {
-      BootstrapServers = "localhost:9092",
+      BootstrapServers = p_bootstrapServers,
       LingerMs = 0,
       BatchSize = 16384,
       Acks = Acks.Leader,
@@ -27,7 +28,7 @@ class Program
     new()
     {
       GroupId = _groupId,
-      BootstrapServers = "localhost:9092",
+      BootstrapServers = p_bootstrapServers,
       AutoOffsetReset = AutoOffsetReset.Earliest,
       EnableAutoCommit = true
     };
